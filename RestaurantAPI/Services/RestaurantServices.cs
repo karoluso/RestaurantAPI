65cee297429981cee7bdccde90fa14e11645becf
karoluso <kalka79@gmail.com>
2022-01-19 19:47:22 +0100

    going to laptop 2

--- RestaurantAPI/Services/RestaurantServices.cs
+++ RestaurantAPI/Services/RestaurantServices.cs
@@ -1,6 +1,54 @@
-﻿namespace RestaurantAPI.Services
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantAPI.Services
 {
     public class RestaurantServices
     {
+        private readonly RestaurantDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public RestaurantServices(RestaurantDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public RestaurantDto GetById(int id)
+        {
+            var restaurant = _dbContext
+                .Restaurants
+                .Include(_r => _r.Address)
+                .Include(_r => _r.Dishes)
+                .FirstOrDefault(r => r.Id == id);
+
+            if (restaurant == null) return null;
+
+            var result = _mapper.Map<RestaurantDto>(restaurant);
+
+            return result;
+        }
+
+        public IEnumerable<RestaurantDto> GetAll()
+        {
+            var restaurants = _dbContext
+                .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
+                .ToList();
+
+            var  restaurantDtos = _mapper.Map<List<RestaurantDto>>(restaurants);
+
+            return restaurantDtos;
+        }
+
+
+        public void CreateRestaurant(RestaurantDto)
     }
 }
