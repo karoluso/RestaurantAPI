c3ec451a6a5ea0aeb27d5d3b37db2c3694e3ff4f
karol kal <karolkal@op.pl>
2022-01-24 21:41:22 +0100

    -going to desktop

--- RestaurantAPI/Entities/RestaurantDbContext.cs
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -13,6 +13,9 @@ namespace RestaurantAPI.Entities
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Dish> Dishes { get; set; }
 
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Restaurant>()
@@ -33,6 +36,15 @@ namespace RestaurantAPI.Entities
                 .Property(r => r.Street)
                 .IsRequired()
                 .HasMaxLength(50);
+
+            modelBuilder.Entity<User>()
+                .Property(_ => _.Emial)
+                .IsRequired();
+
+            modelBuilder.Entity<Role>()
+                .Property(_ => _.Name)
+                .IsRequired();
+
         }
 
         protected override void  OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ RestaurantAPI/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace RestaurantAPI.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Emial { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string nationality { get; set; }
+        public string PassswordHash { get; set; }
+
+        public int RoleiId { get; set; }
+        public virtual Role Role { get; set; }
+
+    }
+}
