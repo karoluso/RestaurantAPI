ab3cd36c9eae659dbe7d8bf9169921e2a7bd6376
karol kal <karolkal@op.pl>
2022-01-24 07:08:39 +0100

    -going to laptop

---
+++ RestaurantAPI/Controllers/DishController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/{restaurantId}/dish")]
+    [ApiController]
+    public class DishController : ControllerBase
+    {
+        [HttpPost("{restaurantId}")]
+        public ActionResult Post([FromRoute] int restaurantId, [FromBody] DishDto dto)
+        {
+            var dish=_mapper
+        }
+    }
+}
