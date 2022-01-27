8688b4a132952b854838b39f7025645fde74740f
karoluso <kalka79@gmail.com>
2022-01-27 14:07:30 +0100

    lecture #35 JWT authentication completed

--- RestaurantAPI/ReadMe.txt
+++ RestaurantAPI/ReadMe.txt
@@ -4,3 +4,18 @@
 
 
 3. swagger dodanie w startup poprzez app, fluent validation dodtanie w startup inaczej, serwisy jeszcze inaczej -0 jak to jest z tym dodawaniemk?
+   Nlog jeszcze inaczej jest dodany, FluentValidation jescze jest dodane do AddControlers
+
+4.dalczego zamiszczamy wlasciwosci authentykacji w appsettings a nie w klasie.
+
+   =========================
+   > public IConfiguration Configuration { get; } it's in a  Startup and using it we can relate to 
+     the appsettings.json;
+     Configuration.GetSection("Authentication").Bind(authenticationSettings)  we bind created class  
+     the properties that corresponds to those written in appsettings.json. GetSection - we need to give a name 
+     of the section from the appsettings.json;
+
+
+
+
+     Doczytac pytanie Co dalej !! z pytan kursu.
--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -2,23 +2,15 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Middleware;
 using RestaurantAPI.Models;
 using RestaurantAPI.Models.Validators;
 using RestaurantAPI.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace RestaurantAPI
 {
@@ -34,6 +26,17 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var authenticationSettings = new AuthenticationSettings();
+
+            Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultScheme = "Bearer";
+                option.DefaultAuthenticateScheme=
+
+            })
+
             services.AddTransient<IWeatherForecastService, WeatherForecastService>();
             services.AddControllers().AddFluentValidation();
             services.AddDbContext<RestaurantDbContext>();
@@ -53,6 +56,8 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env,RestaurantSeeder seeder)
         {
+            ValidatorOptions.Global.LanguageManager.Enabled = false;  // just to have all messeges in english
+
             seeder.Seed();
 
             if (env.IsDevelopment())
