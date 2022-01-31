d2fb385c02e99c774ceef401a0bd3df38fab6e8b
karoluso <kalka79@gmail.com>
2022-01-31 21:08:23 +0100

    lecture #38 authorization by  user role  compl.

--- RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -45,6 +45,7 @@ namespace RestaurantAPI.Controllers
 
 
         [HttpPost]
+        [Authorize(Roles ="Admin,Manager")]
         public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
         {
             int id = _restaurantService.CreateRestaurant(dto);
