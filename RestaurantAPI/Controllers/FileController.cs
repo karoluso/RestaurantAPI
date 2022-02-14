297636c04a60f16e21bf9e2d302641e45d8014b2
karoluso <kalka79@gmail.com>
2022-02-14 17:24:57 +0100

    #52 uploading files to server , completed

--- RestaurantAPI/Controllers/FileController.cs
+++ RestaurantAPI/Controllers/FileController.cs
@@ -10,6 +10,7 @@ namespace RestaurantAPI.Controllers
     [Authorize]
     public class FileController : ControllerBase
     {
+        [HttpGet]
         public ActionResult GetFile([FromQuery] string filename)
         {
             var rootPath=Directory.GetCurrentDirectory();
@@ -25,10 +26,26 @@ namespace RestaurantAPI.Controllers
             var contentProvider = new FileExtensionContentTypeProvider();
             contentProvider.TryGetContentType(filePath, out string contentType);
 
-
-
             return File(fileContents, contentType, filename);
 
+        }
+
+        [HttpPost]
+        public ActionResult Upload([FromForm]IFormFile file)
+        {
+            if(file!=null && file.Length > 0)
+            {
+               var  rootPath = Directory.GetCurrentDirectory();
+                var fileName = file.FileName;
+                var fullePath = $"{rootPath}/PrivateFiles/{fileName}";
+                using (var stream = new FileStream(fullePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return Ok();
+            }
+
+            return BadRequest();
         }
     }
 }
