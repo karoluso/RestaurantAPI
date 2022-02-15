a8414e0ec559e32900c23172c1392e0f3cfa4c65
karol kal <karolkal@op.pl>
2022-02-15 13:16:45 +0100

    #60 Migration to .Net 6.0, completed

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,22 +1,147 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NLog.Web;
+using RestaurantAPI;
+using RestaurantAPI.Authorization;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Middleware;
+using RestaurantAPI.Models;
+using RestaurantAPI.Models.Validators;
+using RestaurantAPI.Services;
+using System.Reflection;
+using System.Text;
 
-namespace RestaurantAPI
-{
-    public class Program
+//namespace RestaurantAPI
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            CreateHostBuilder(args).Build().Run();
+//        }
+
+//        public static IHostBuilder CreateHostBuilder(string[] args) =>
+//            Host.CreateDefaultBuilder(args)
+//                .ConfigureWebHostDefaults(webBuilder =>
+//                {
+//                    webBuilder.UseStartup<Startup>();
+//                })
+//            .UseNLog();
+//    }
+//}
+
+var builder = WebApplication.CreateBuilder();
+
+//NLog: Setup NLog for Dependency Injection
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+builder.Host.UseNLog();
+
+
+// configure builder.Services, Use this method to add builder.Services to the container.
+
+    var authenticationSettings = new AuthenticationSettings();
+
+    builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+    builder.Services.AddSingleton(authenticationSettings);
+
+    builder.Services.AddAuthentication(option =>
     {
-        public static void Main(string[] args)
+        option.DefaultAuthenticateScheme = "Bearer";
+        option.DefaultScheme = "Bearer";
+        option.DefaultChallengeScheme = "Bearer";
+    }).AddJwtBearer(cfg =>
+    {
+        cfg.RequireHttpsMetadata = false;
+        cfg.SaveToken = true;
+        cfg.TokenValidationParameters = new TokenValidationParameters
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            ValidIssuer = authenticationSettings.JwtIssuer,
+            ValidAudience = authenticationSettings.JwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+        };
+    });
+    builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy("HasNationality", builder => builder.RequireClaim("Nationality", "German", "Polish"));
+        options.AddPolicy("HasMinimumAge20", builder => builder.AddRequirements(new MinimumAgeRequirement(20)));
+        options.AddPolicy("CreatedMinimum2Restaurants", builder => builder.AddRequirements(new MinimumCreatedResourcesRequirement(2)));
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-            .UseNLog();
-    }
+    builder.Services.AddScoped<IAuthorizationHandler, MinimumAgeRequirementHandler>();
+    builder.Services.AddScoped<IAuthorizationHandler, ResourceOperationRequirementHandler>();
+    builder.Services.AddScoped<IAuthorizationHandler, MinimumCreatedRsourcesRequirementHandler>();
+
+    builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
+    builder.Services.AddControllers().AddFluentValidation();
+
+    builder.Services.AddScoped<RestaurantSeeder>();
+    // builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    builder.Services.AddTransient<IRestaurantService, RestaurantServices>();
+    builder.Services.AddTransient<IDishService, DishService>();
+    builder.Services.AddTransient<IAccountService, AccountService>();
+    builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+    builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+    builder.Services.AddScoped<IValidator<RestaurantQuery>, RestaurantQueryValidator>();
+    builder.Services.AddScoped<ErrorHandlingMiddleware>();
+    builder.Services.AddScoped<RequestTimeMiddleware>();
+    builder.Services.AddScoped<IUserContextService, UserContextService>();
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("FrontEndClient", builderPolicy =>
+
+            builderPolicy.AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins(builder.Configuration["AllowedOrigins"])
+        );
+    });
+    builder.Services.AddDbContext<RestaurantDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantDbConnection")));
+
+
+var app= builder.Build();
+// configure. Use this configure the HTTP request pipeline.
+
+ValidatorOptions.Global.LanguageManager.Enabled = false;  // just to have all messeges in english
+
+var scope= app.Services.CreateScope();
+var seeder=scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+
+app.UseResponseCaching();
+app.UseStaticFiles();
+app.UseCors("FrontEndClient");
+
+seeder.Seed();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseMiddleware<RequestTimeMiddleware>();
+app.UseAuthentication();
+app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API");
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
